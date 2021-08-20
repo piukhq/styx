5e06c116c56755d20d9acdb545829f0d847ff484
Chris Pressland <mail@cpressland.io>
2021-08-20 17:25:25 +0100

    Added basic SFTP Client

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 > Personification of Hatred
 
 Downloads files from Mastercard and uploads then to Azure Blob Storage
+
+```
+Testing: sftp -i id_mastercard -P 16022 Z218502@mtf.files.mastercard.com
+         | Contains: /0073185/test/download/TGX4
+Production: sftp -i id_mastercard -P 16022 Z216458@files.mastercard.com
+         | Contains: /0073185/production/download/TGX2
+File Express: sftp -i id_mastercard -P 16022 Z218502@files.mastercard.com
+         | Contains: /0073185/production/download/TS44
+         |           /0073185/production/download/TGX2
+```
--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("Hello, World!")
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+from io import StringIO
+import json
+import paramiko
+import logging
+
+logging.getLogger('azure.identity').setLevel(logging.ERROR)
+
+
+def get_sftp_key() -> str:
+    vault_url = "https://bink-uksouth-prod-com.vault.azure.net/"
+    vault_credential = DefaultAzureCredential()
+    vault_client = SecretClient(vault_url=vault_url, credential=vault_credential)
+    key = json.loads(vault_client.get_secret('mastercard-sftp').value)['key']
+    return StringIO(key)
+
+
+def sftp_client(host: str, port: int, username: str) -> paramiko.SFTPClient:
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    key = paramiko.RSAKey.from_private_key(get_sftp_key())
+    ssh.connect(host, port=port, username=username, pkey=key)
+    return ssh.open_sftp()
+
+
+# s = sftp_client(host="mtf.files.mastercard.com", port=16022, username="Z218502")
+# s = sftp_client(host="files.mastercard.com", port=16022, username="Z216458")
+# s = sftp_client(host="files.mastercard.com", port=16022, username="Z218502")
